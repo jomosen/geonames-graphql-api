f346b8944e799e63121f2eccd726427553fcfc96
jomosen <josemorellsendra@gmail.com>
2025-12-08 10:12:40 +0100

    Delete files

--- src/geonames/infrastructure/persistence/models/admin_geoname_model.py
+++
@@ -1,6 +0,0 @@
-from geonames.infrastructure.persistence.models.geonames_base_model import GeonamesBaseModel
-
-
-class AdminGeonameModel(GeonamesBaseModel):
-
-    __tablename__ = "admin_geonames"
--- src/geonames/infrastructure/persistence/models/city_geoname_model.py
+++
@@ -1,6 +0,0 @@
-from geonames.infrastructure.persistence.models.geonames_base_model import GeonamesBaseModel
-
-
-class CityGeonameModel(GeonamesBaseModel):
-
-    __tablename__ = "city_geonames"
